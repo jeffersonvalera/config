b4e0c78a9e3a39d972ab9dd21b008ffba2d9a0b7
Jefferson Valera <jefvaler@publicisgroupe.net>
2024-01-16 10:49:38 -0600

    Restore rust debug

---
+++ nvim/lua/plugins/debug.lua
@@ -0,0 +1,77 @@
+--[[ local bindings = function(dap, dapui)
+	vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Debug - Toggle Break Point" })
+	vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "Debug - Continue/Start" })
+
+	vim.keymap.set("n", "<leader>dt", dapui.toggle, { desc = "Debud - Toggle Debugger View" })
+	dap.listeners.before.attach.dapui_config = function()
+		dapui.open()
+	end
+
+	dap.listeners.before.launch.dapui_config = function()
+		dapui.open()
+	end
+
+	dap.listeners.before.event_terminated.dapui_config = function()
+		dapui.close()
+	end
+
+	dap.listeners.before.event_exited.dapui_config = function()
+		dapui.close()
+	end
+end
+
+local init_adapters = function(dap)
+	dap.adapters.lldb = {
+		type = "executable",
+		command = "/Users/jefvaler/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/lldb/bin/lldb", -- adjust as needed, must be absolute path
+		name = "lldb",
+	}
+	--[[ dap.adapters.lldb = {
+		type = "server",
+		host = "127.0.0.1", -- adjust as needed, must be absolute path
+		port = "13000",
+	} ]]
+--[[ end
+local rust_config = function(dap)
+	dap.configurations.rust = {
+		{
+			name = "Launch",
+			type = "lldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+			args = {},
+
+			-- 💀
+			-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+			--
+			--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+			--
+			-- Otherwise you might get the following error:
+			--
+			--    Error on launch: Failed to attach to the target process
+			--
+			-- But you should be aware of the implications:
+			-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+			-- runInTerminal = false,
+		},
+	}
+end ]]
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		dapui.setup()
+
+		bindings(dap, dapui)
+		init_adapters(dap)
+		rust_config(dap)
+	end,
+}
--- nvim/lua/plugins/rust-tools.lua
+++ nvim/lua/plugins/rust-tools.lua
@@ -1,9 +1,23 @@
 return {
   {
     "simrat39/rust-tools.nvim",
+    dependencies = {
+      {
+        "mrcjkb/rustaceanvim",
+        version = "^3", -- Recommended
+        ft = { "rust" },
+      },
+    },
     config = function()
       local rt = require("rust-tools")
 
+      local extension_path = vim.env.HOME .. "/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/"
+      local codelldb_path = extension_path .. "adapter/codelldb"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+
+      local cfg = require("rustaceanvim.config")
+
+      print(cfg.get_codelldb_adapter(codelldb_path, liblldb_path).name)
       rt.setup({
         server = {
           on_attach = function(_, bufnr)
@@ -13,13 +27,16 @@ return {
             vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
           end,
         },
+        dap = {
+          adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
       })
-    end
+    end,
   },
   {
     "rust-lang/rust.vim",
-    config = function ()
+    config = function()
       vim.g.rustfmt_autosave = 1
-    end
-  }
+    end,
+  },
 }
